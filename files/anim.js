155ae282cc989c754bd773471adaf5fc0eb7d74f
Victor Tang <victortang1992@gmail.com>
2015-09-06 12:42:09 -0700

    init Jquery, Angular

---
+++ files/anim.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){ 
+    $(function() {
+    var div = $('.dynamicheight');
+    var width = div.width(); 
+    div.css("height", width/2);
+    });
+});
+
--- files/app.js
+++ files/app.js
@@ -1,15 +1,14 @@
 (function() {
     //start of function
-  var app = angular.module('formatter', ['filters']);
+  var app = angular.module('formatter', []);
   app.factory('linesFile', function() {
     var linesFile = {};
-    linesFile.monstall = monstall;
-    linesFile.modelObject = {};
+    //linesFile.monstall = monstall;
+    //linesFile.modelObject = {};
     return linesFile;
   }); //end of service
   app.controller('lineDisplay',['$scope','linesFile', function($scope, linesFile) {
-//monsterDb -> get database, get model object
-    $scope.linesFile = linesFile;
+
     }]) //end of controller
   //end of function
 })();
