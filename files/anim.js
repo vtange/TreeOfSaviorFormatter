c2931822ed84eec39c71344fb03c50bf269476bf
Victor Tang <victortang1992@gmail.com>
2015-09-06 14:18:37 -0700

    Working height and width resizing. Cleaner code.

--- files/anim.js
+++ files/anim.js
@@ -1,8 +1,24 @@
-$(document).ready(function(){ 
-    $(function() {
-    var div = $('.dynamicheight');
-    var width = div.width(); 
-    div.css("height", width/2);
-    });
+$(document).ready(function(){
+    function jqUpdateSize(){
+    // Get the dimensions of the viewport
+    var width = $(document).width();    //used for repositioning, width used to take advantage of css:contain
+        //for every element, make a height to width ratio.
+    var div1 = $('.left-bar');
+    var width1 = div1.width();
+    div1.css("height", width1*2.28);
+    var div2 = $('.drop-zone');
+    var width2 = div2.width();
+    div2.css("height", width2*0.56);
+    var div3 = $('.right-bar');
+    var width3 = div3.width();
+    div3.css("top", width*0.22);
+    div3.css("height", width3*1.3);
+    var div4 = $('.text-block');
+    var width4 = div4.width();
+    div4.css("top", width*0.418);
+    div4.css("height", width4*0.23);
+};
+$(document).ready(jqUpdateSize);    // When the page first loads
+$(window).resize(jqUpdateSize);     // When the browser changes size
 });
 
