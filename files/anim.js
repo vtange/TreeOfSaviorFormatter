7baeac21aaddaceda5723373a84b05b64249482b
Victor Tang <victortang1992@gmail.com>
2015-09-17 00:47:52 -0700

    add text block popup file

--- files/anim.js
+++ files/anim.js
@@ -22,11 +22,11 @@ $(document).ready(function(){
     div3.css("height", width3*1.3);
     var div4 = $('.text-block');
     var width4 = div4.width();
-    div4.css("top", width*0.42);
-    div4.css("height", width4*0.23);
+    div4.css("top", width*0.41);
+    div4.css("height", width4*0.26);
     var div5 = $('.dialog');
     var width5 = div5.width();
-    div5.css("top", width*0.0142);
+    div5.css("top", width*0.024);
     div5.css("height", width5*0.1);
 };
 $(document).ready(jqUpdateSize);    // When the page first loads
