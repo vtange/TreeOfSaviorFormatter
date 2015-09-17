5fb6f18038ae872740bc5ae67d9db79c905cae57
Victor Tang <victortang1992@gmail.com>
2015-09-17 16:51:24 -0700

    dialog works now, better scaling

--- files/anim.js
+++ files/anim.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-    $('body').flowtype({
+    $('.dialog').flowtype({
        minimum   : 10,
        maximum   : 9000,
        minFont   : 1,
        maxFont   : 18.25,
-       fontRatio : 100
+       fontRatio : 110
+    });
+    $('.right-bar').flowtype({
+       minimum   : 10,
+       maximum   : 120,
+       minFont   : 1,
+       maxFont   : 30.25,
+       fontRatio : 30
     });
     function jqUpdateSize(){
     // Get the dimensions of the viewport
@@ -18,7 +25,7 @@ $(document).ready(function(){
     div2.css("height", width2*0.56);
     var div3 = $('.right-bar');
     var width3 = div3.width();
-    div3.css("top", width*(-0.35));
+    div3.css("top", width*(-0.341));
     div3.css("height", width3*1.3);
     var div4 = $('.text-block');
     var width4 = div4.width();
@@ -26,7 +33,7 @@ $(document).ready(function(){
     div4.css("height", width4*0.26);
     var div5 = $('.dialog');
     var width5 = div5.width();
-    div5.css("top", width*0.024);
+    div5.css("top", width*0.026);
     div5.css("height", width5*0.1);
 };
 $(document).ready(jqUpdateSize);    // When the page first loads
