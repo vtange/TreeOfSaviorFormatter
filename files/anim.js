083aabce004a6d41a29621796d82beb561d46eda
Victor Tang <victortang1992@gmail.com>
2015-09-17 18:23:14 -0700

    add Cabin, got right side working, (better than dialog) work on dialog again

--- files/anim.js
+++ files/anim.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-    $('.dialog').flowtype({
-       minimum   : 10,
-       maximum   : 9000,
-       minFont   : 1,
-       maxFont   : 18.25,
-       fontRatio : 110
-    });
-    $('.right-bar').flowtype({
-       minimum   : 10,
-       maximum   : 120,
-       minFont   : 1,
-       maxFont   : 30.25,
-       fontRatio : 30
-    });
     function jqUpdateSize(){
     // Get the dimensions of the viewport
     var width = $(window).width();    //used for repositioning, width used to take advantage of css:contain
@@ -20,21 +6,40 @@ $(document).ready(function(){
     //var div1 = $('.left-bar');
     //var width1 = div1.width();
     //div1.css("height", width1*2.28);
+        
     var div2 = $('.drop-zone');
     var width2 = div2.width();
     div2.css("height", width2*0.56);
+        
     var div3 = $('.right-bar');
     var width3 = div3.width();
-    div3.css("top", width*(-0.341));
+    div3.css("top", width*(0.222));
     div3.css("height", width3*1.3);
+        
     var div4 = $('.text-block');
     var width4 = div4.width();
     div4.css("top", width*0.41);
     div4.css("height", width4*0.26);
+        
     var div5 = $('.dialog');
     var width5 = div5.width();
     div5.css("top", width*0.026);
     div5.css("height", width5*0.1);
+        
+    $('.dialog').flowtype({
+       minimum   : 10,
+       maximum   : 9000,
+       minFont   : 1,
+       maxFont   : 18.25,
+       fontRatio : 110
+    });
+    $('.quest').flowtype({
+       minimum   : 10,
+       maximum   : 9000,
+       minFont   : 1,
+       maxFont   : 48.25,
+       fontRatio : 200
+    });
 };
 $(document).ready(jqUpdateSize);    // When the page first loads
 $(window).resize(jqUpdateSize);     // When the browser changes size
