ce6684d2231f68908afafba9c8b04b42d2e823c4
Victor Tang <victortang1992@gmail.com>
2015-09-20 00:24:10 -0700

    angular - toggle aides

--- files/app.js
+++ files/app.js
@@ -10,5 +10,33 @@
   app.controller('lineDisplay',['$scope','linesFile', function($scope, linesFile) {
 
     }]) //end of controller
+  app.controller('aideCtrl',['$scope','linesFile', function($scope, linesFile) {
+//toggle text block
+      this.txtblock = 0;
+      this.toggle = function(){
+        if(this.txtblock == 0){
+            return this.txtblock = 1;
+        }
+        else {
+            return this.txtblock = 0;
+        }
+      };
+      this.toggledOn = function(){
+          return this.txtblock === 1;
+      };
+//toggle right bar
+      this.rbar = 0;
+      this.toggle2 = function(){
+        if(this.rbar == 0){
+            return this.rbar = 1;
+        }
+        else {
+            return this.rbar = 0;
+        }
+      };
+      this.toggledOn2 = function(){
+          return this.rbar === 1;
+      };
+    }]) //end of controller
   //end of function
 })();
